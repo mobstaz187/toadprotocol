54e7b0cfa4f94f3ed504534228cf7827b614cc73
mobstaz187 <markmark092100@gmail.com>
2024-12-12 07:31:46 +0800

    Add files via upload

---
+++ command.js
@@ -0,0 +1,234 @@
+var youtube = "https://www.youtube.com/watch?v=YKPMeqPEurA";
+var twitter = "https://x.com/toadprotocol";
+var contractaddress = "placeholder_for_updating";
+var password = "ribbit";
+var dex = "Transmitting TXN"
+
+transmission = [
+  "<br>",
+  ".... . .-.. .-.. --- / .--- ..- -. ---",
+  ".-- . .-.. -.-. --- -- . / - --- / ... .--. .- -.-. .",
+  "-.-- --- ..- / .- .-. . / -- .- -. -.- .. -. -.. .----. ... / .-.. .- ... - / .... --- .--. .",
+  "... .- ...- . / ..- ... .-.-.- .-.-.- .-.-.- .-.-.-",
+  "<br>"
+];
+aboutme = [
+  "<br>",
+  "I am Goliath, as I've told I am the \"last hope\". I dont know what it meant. Yet, I know my purpose",
+  "<br>",
+  "****GROAN**** I came to space on a mission. It is yet to be transmitted. But I know that it is very important.",
+  "toad Messiah, please guide me. [faint bark]",
+  "<br>",
+  "It's cold, dark, and lonely. I wonder what is in the vastness of space.",
+];
+
+dex = [
+  "<br>",
+  'Initial Contact: Dex        <a href="' + dex + '" target="_blank">Updating...' + '</a>',
+  "<br>"
+];
+ca = [
+    "<br>",
+    '<a class="color1" title="Copy Text" id="text" onclick="copyElementText(this.id)">Ongoing Transmission</a>',
+    "<br>"
+  ];
+ribbit = [
+  "<br>",
+  "$Goliath",
+  '𝕏        <a href="' + twitter + '" target="_blank">twitter/toadprotocol' + '</a>',
+  "contract address (Click to Copy): ticker $Goliath",
+  '<a class="color1" title="Copy Text" id="text" onclick="copyElementText(this.id)">Ongoing Transmission</a>',
+  "<br>"
+];
+social = [
+  "<br>",
+  '𝕏        <a href="' + twitter + '" target="_blank">https://x.com/toadprotocol' + '</a>',
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline or confidential.",
+  "<br>"
+];
+
+search = [
+'<span id="serts"></span>',
+];
+
+launch = [
+  "<br>",
+  '<h1 class="index">Launch sequence initiated....</h1>',
+  '<span class="command2">Command:</span> <span class="index">Go ahead. Start on the switch.</span>',
+  '<span class="color2">Goliath:</span> <span class="index">Now, the drink... ribbit...</span>',
+  '<span class="command2">Command:</span> <span class="index">Alright, LMP? Okay. Got the drink, got the gun.</span>',
+  '<span class="color2">Goliath:</span> <span class="index">Right, go ribbit ahead.</span>',
+  '<span class="command2">Command:</span> <span class="index">Panel 3.</span>',
+  '<span class="color2">Goliath:</span> <span class="index">ribbit, go ahead.</span>',
+  '<span class="command2">Command:</span> <span class="index">Fuel Cell B Main Bus B switch on up release.</span>',
+  '<span class="command2">Command:</span> <span class="index">Fuel Cell 2 Main B on up release. Verify.</span>',
+  '<span class="color2">Goliath:</span> <span class="index">Confirribbittion. Master alarm?</span>',
+  '<span class="command2">Command:</span> <span class="index">Roger.</span>',
+  '<span class="command2">Command:</span> <span class="index">Right. CDR, STC. How do you read me?</span>',
+  '<span class="color2">Goliath:</span> <span class="index">ribbited and Clear STC....</span>',
+  '<span class="color2">Goliath:</span> <span class="index"Disk program Loaded. Terminal running.</span>',
+  '<span class="command2">Command:</span> <span class="index">Program 2?</span>',
+  '<span class="color2">Goliath:</span> <span class="index">Affirribbittive.</span>',
+  '<span class="command2">Command:</span> <span class="index">Stand by.</span>',
+  '<span class="command3">Speaker:</span> <span class="index">10, 9, 8, 7, 6....</span>',
+  '<span class="color2">Goliath:</span> <span class="index">**RIBBIT**</span>',
+  '<span class="command3">Speaker:</span> <span class="index">5, 4, 3, 2, 1.....</span>',
+  '<span class="command2">Command:</span> <span class="index">Ignition, Lift-off...</span>',
+  '<span class="color2">Goliath:</span> <span class="index">ribbit, Roger. We got a roll program.</span>',
+  '<span class="command2">Command:</span> <span class="index">Roger, roll...</span>',
+  '<span class="color2">Goliath:</span> <span class="index">ribbit.... Inboard cut-off. Tower is gone.</span>',
+  '<span class="command2">Command:</span> <span class="index">Goliath, you are a go [Long pause.]</span>',
+  '<span class="color2">Goliath:</span> <span class="index">ribbit, okay. Thank you.</span>',
+  "<br>"
+];
+
+earthorbit = [
+  "<br>",
+  '<h1 class="index">Lift-off success. Current position: Earth Orbit....</h1>',
+  '<span class="command2">Command:</span> <span class="index">Comm tech..</span>',
+  '<span class="color2">Goliath:</span> <span class="index">Comribbit tech established.</span>',
+  '<span class="command2">Command:</span> <span class="index">Goliath, this is mission control.</span>',
+  '<span class="color2">Goliath:</span> <span class="index">ribbit, reading you loud and clear.</span>',
+  '<span class="command2">Command:</span> <span class="index">I would like to pass up your Delta Correction. </span>',
+  '<span class="color2">Goliath:</span> <span class="index">Stand by......***GROANS***</span>',
+  '<span class="color2">Goliath:</span> <span class="index">Go ahead.</span>',
+  '<span class="command2">Command:</span> <span class="index">Delta correction is plus 2.2. We do recommend a P52 alignment over.</span>',
+  '<span class="color2">Goliath:</span> <span class="index">Go ahead, thrusters compensating.... ribbit...</span>',
+  '<span class="command2">Command:</span> <span class="index">Shuttle compensating. Affirmative.</span>',
+  '<span class="color1"></span> <span class="index">****Self monologue*****</span>',
+  '<span class="color2">Goliath:</span> <span class="index">ribbit, I need some lights.</span>',
+  '<span class="color2">Goliath:</span> <span class="index">it feels like I am trapped.</span>',
+  '<span class="color2">Goliath:</span> <span class="index">DSKY 52%</span>',
+  '<span class="color2">Goliath:</span> <span class="index">Maintaining 39 degrees. ribbit-ike.</span>',
+  '<span class="color1"></span> <span class="index">*****Operator comes back******</span>',
+  '<span class="command2">Command:</span> <span class="index">This is Comm Tech.</span>',
+  '<span class="color2">Goliath:</span> <span class="index">Loud and clear. ***GROANS***</span>',
+  '<span class="command2">Command:</span> <span class="index">S-band radio check.</span>',
+  '<span class="color2">Goliath:</span> <span class="index">Check.</span>',
+  '<span class="command2">Command:</span> <span class="index">GO...O</span>',
+  '<span class="color2">Goliath:</span> <span class="index">You are breaking up. ribbit.</span>',
+  '<span class="command2">Command:</span> <span class="index">[Long Silence].</span>',
+  '<span class="index">Initiating Transmission......</span>',
+  '<span class="color2">Goliath:</span> <span class="index">Can anybody hear ribbit?</span>',
+  '<span class="command2">Command:</span> <span class="index">Hello Goliath, you are now a space toad.</span>',
+  '<span class="color2">Goliath:</span> <span class="index">ribbit, where am I?</span>',
+  '<span class="command2">Command:</span> <span class="index">Welcome to SPACE.</span>',
+  '<span class="color2">Goliath:</span> <span class="index">ribbit! What is my DIRECTIVE?</span>',
+  '<span class="command2">Command:</span> <span class="index">Surv..ive....Mo...on....Re....urn...Sa..f.e..</span>',
+  '<span class="color2">Goliath:</span> <span class="index">Control, you are breaking up.</span>',
+  '<span class="command2">Command:</span> <span class="index">GO...LIATH...Hell...o...Are....You..the...</span>',
+  '<span class="color2">Goliath:</span> <span class="index">Control?</span>',
+  '<span class="color2">Goliath:</span> <span class="index">ribbit?</span>',
+  '<span class="color2">Goliath:</span> <span class="index">Affirribbittive.</span>',
+  "<br>",
+];
+
+logs = [
+  '<h1 class="index">Declassifying mission log.</h1>',
+  '<span class="color2">Day 1:</span> <span class="index">Launch</span>',
+  '<span class="color2">Day 1:</span> <span class="index">Earth Orbit</span>',
+  '<span class="color2">Day 1:</span> <span class="index">Delta Correction</span>',
+  '<span class="color2">Day 1:</span> <span class="index">Lost Transmission</span>',
+  '<span class="index">*After Communitoadion is Restored.</span>',
+  '<span class="color2">Day 2:</span> <span class="index">Directives</span>',
+  '<span class="color2">Day 2:</span> <span class="index">Exploration</span>',
+  '<span class="color2">Day 3:</span> <span class="index">Findings</span>',
+  '<span class="color2">Day 4:</span> <span class="index">Lost Blockchain Technology</span>',
+  '<span class="color2">Day 4:</span> <span class="index">Poking the Lost Blockchain Technology</span>',
+  '<span class="color2">Day 4:</span> <span class="index">Experimentation on the Blockchain Technology</span>',
+  '<span class="color2">Day 5:</span> <span class="index">The Discovery</span>',
+  "<br>",
+];
+
+journal = [
+  "<br>",
+  '<span class="color2">Goliath Flight:</span> <span class="index">First Part of the Mission</span>',
+  '<span class="color2">Goliath in Orbit:</span> <span class="index">Anomalies in Orbit</span>',
+  '<span class="color2">Comm-Tech:</span> <span class="index">Lost Transmission</span>',
+  '<span class="color2">Technology:</span> <span class="index">Blockchain Discovery</span>',
+  '<span class="color2">Exploration:</span> <span class="index">The Discovery</span>',
+  "<br>",
+];
+
+help = [
+  "<br>",
+  '<span class="command">transmission</span>   Show encrypted message',
+  '<span class="command">aboutme</span>        Who is Goliath',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">CA</span>             Contract Address',
+  '<span class="command">ribbit</span>         Try me.',
+  '<span class="command">logs</span>           View Goliath Mission Logs',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">home</span>           Display the Homescreen',
+  '<span class="command">help</span>           Show available Commands',
+  "<br>",
+  '<h1 class="command2">scripts (./script.sh)</h1>',
+  '<div class="command1"> log1.sh </div></br>',
+  '<div class="command1"> log2.sh </div></br>',
+  '<div class="command1"> log3.sh </div></br>',
+];
+
+date = [
+  '<p id="date1"></p>',
+];
+
+banner = [
+'<span class="index">Initiating TOAD Protocol</span>',
+'<span class="index">INCOMING.... $GOLIATH</span>',
+'                  ####',
+'            #    #    ########',
+'           #  ### ####XXXXXXXX######    ####',
+'            X ## # XXXXXXXXXXXXXXXXX##########',
+'         ## ##   #X       XXXXXXXXXXXXX#XXXX####',
+'       ##  X#   ##XXXXXXXX      XXXXXXXXXXXXXX#X#',
+'     ##  XXXX#####XXXXXXXXXXXXXX              X#X##',
+'    #  XXXXXXX##XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX   XX#',
+'  #  XXXXXXXXX#XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX  X##',
+' #X##XXXXXXXXX####XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX XX#',
+' # XXXXXXXXXXXXXXX########XXXXXXXXXXXXXXXXXXXXXXXXXX  X#',
+' # XXXXXXXXX######XXXXXXXX#XXXXXXXXXXXXXXXXXXXXXXXXXXX X##   ########',
+'  ##########X X X XXXXXXXX#XXXXXXXXXXXXXXXXXXXXXXXXXXXX XX###XXXXXXXX#',
+'   X X X X X X X X X X #####XXXXXXXXXXXXXXXXXXXXXXXXXXXX  XX##XXXXXXXX#',
+'     #X X X X X XXXX#XXXXXXX####XXXXXXXXXXXXXXXXXXXXXXXXXX XXX##XXXXXX#',
+'       X X X X X XXXXX X X XXXXX##XXXXXXXXXXXXXXXXXXXXXXXXX  XXX#XXXX#',
+'        X X X X XX#X#XXXXXXXXXXXXX#XXXXXXXXXXXXXXXXXX########X########',
+'              #X X XXX X X X X XXX#XXXXXXXXXXXXXX###XXXXXXXXX#X#XXXXX#',
+'               #XXX##X#XXXXXXXXXXX#XXXXXXXXXXX###X#X#X#XXXXXXX#X#XXXX#',
+'               # X X##XXXXXX X X XX#XXXXXXXX##X##XX#X#X#XXXXXXX#XXX#XX#',
+'               ####X#X#####XXXXXXXX#XXXXXX##X##XXXXXXX#X#X#XXX#XXX#XX##',
+'              #X # # X X #XX X XXX#XXXX###XX#XXXXXXXX#X#X#XXX#XXX####',
+'             ##XXX### X X#XXXXXX##XXX##XXX#XXXXXXXXX#X#X#XXXXXX###',
+'          ###X X ### # X#X X XX#XXX##XXXX#X#XXXXXXXXX#X#XXXXX####',
+'       ###XXXXXX########XXXXXX# XXX#XXX##XXX#XXXXXXX#X#XXX##XX#X#',
+'    ############       X X #X# X X #X##XXXX#XXXXXXX#X#X#X#X#XX#X',
+'      ###     ##      #XXX#X# X X #XXXXXXX#X#XXXXX#X###XXX#XX#X',
+'     # #     ##      # X #X# X X X#XXXXXX#X#XXXXXXX#XXXXXXX##X',
+'      #            ##XXX#X# X X X #XXXXX#X#XXXXX###X#XXXXX##X',
+'             ######X X #X#   X X X#XXXX#X#X#X###XXX#X##### X',
+'              ##X#XXXX###      #X X##X#X#X###X#X#X###XX X',
+'       ########XXX## X##################XXXXXX##X# X X',
+'       ####  #########################X#######X X X',
+'           ###          ###############',
+'                             ###',
+
+'</br>',
+'<span class="color2">Hello World....</span>',
+  '<span class="color2">Welcome to TOAD interactive protocol</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
+
+
+
+                                                                                            
